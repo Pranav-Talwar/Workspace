c156908f77b4a87a7c3f2ac1ef9c390034399758
Pranav-Talwar <talwarpranav929@gmail.com>
2025-01-08 12:43:21 -0700

    z

--- Semester 3/KASHANK/src/Question.java
+++ Semester 3/KASHANK/src/Question.java
@@ -1,4 +1,87 @@
-package PACKAGE_NAME;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class question {
+/**
+ *
+ * @author
+ */
+public class Question {
+    private String question;
+    private String firstAnswer;
+    private String secondAnswer;
+    private String thirdAnswer;
+    private String fourthAnswer;
+    private String correctAnswer;
+
+
+    public Question() {}
+
+    public Question(String question, String firstAnswer, String secondAnswer, String thirdAnswer, String fourthAnswer,
+                    String correctAnswer) {
+        super(); //superclass method(s)
+        this.question = question;
+        this.firstAnswer = firstAnswer;
+        this.secondAnswer = secondAnswer;
+        this.thirdAnswer = thirdAnswer;
+        this.fourthAnswer = fourthAnswer;
+        this.correctAnswer = correctAnswer;
+    }
+
+    // getter and setter
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getFirstAnswer() {
+        return firstAnswer;
+    }
+
+    public void setFirstAnswer(String firstAnswer) {
+        this.firstAnswer = firstAnswer;
+    }
+
+    public String getSecondAnswer() {
+        return secondAnswer;
+    }
+
+    public void setSecondAnswer(String secondAnswer) {
+        this.secondAnswer = secondAnswer;
+    }
+
+    public String getThirdAnswer() {
+        return thirdAnswer;
+    }
+
+    public void setThirdAnswer(String thirdAnswer) {
+        this.thirdAnswer = thirdAnswer;
+    }
+
+    public String getFourthAnswer() {
+        return fourthAnswer;
+    }
+
+    public void setFourthAnswer(String fourthAnswer) {
+        this.fourthAnswer = fourthAnswer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    // toString method for print the question
+    @Override
+    public String toString() {
+        return question + "\n" + firstAnswer + "\n" + secondAnswer + "\n" + thirdAnswer + "\n" + fourthAnswer;
+    }
 }
